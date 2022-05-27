2ef4a1d57dd1338aa8eab8641b4a86c475ce4c59
Dominux <kir.gimranov@gmail.com>
2022-05-27 16:45:15 +0300

    Added enter clicking handling and some bug fixes

--- client/src/components/JoinRoomForm.svelte
+++ client/src/components/JoinRoomForm.svelte
@@ -1,30 +1,46 @@
-<script>
+<script lang="ts">
 	import { Button, Loading, TextField } from 'attractions'
 	import api from '../api'
 
 	let playerName = ''
 	let isLoading = false
+	let isCreated = false
+
+	$: isDisabled = isCreated || isLoading
 
 	async function joinRoom() {
 		isLoading = true
-		await api.joinRoom(playerName).catch((error) => alert(error))
+		const player = await api.joinRoom(playerName).catch((error) => alert(error))
 		playerName = ''
 		isLoading = false
+
+		if (player) isCreated = true
+	}
+
+	async function handleOnKeyDown(event: CustomEvent) {
+		if (!isDisabled && event.detail.nativeEvent.key === 'Enter') await joinRoom()
 	}
 </script>
 
-<div class="form">
-	<TextField
-		maxlength="16"
-		label="Player name"
-		placeholder="Ur mom"
-		bind:value={playerName}
-		outline
-	/>
-	<Button disabled={isLoading} filled on:click={joinRoom}
-		>{#if isLoading}<Loading />{:else}Join room{/if}</Button
-	>
-</div>
+{#if !isDisabled}
+	<div class="form">
+		<TextField
+			maxlength={16}
+			label="Player name"
+			placeholder="Ur mom"
+			bind:value={playerName}
+			outline
+			on:keydown={handleOnKeyDown}
+		/>
+		<Button disabled={isDisabled || playerName.length < 1} filled on:click={joinRoom}>
+			{#if isLoading}
+				<Loading />
+			{:else}
+				Join room
+			{/if}
+		</Button>
+	</div>
+{/if}
 
 <style>
 	:global(.form) {
--- client/src/components/SituationCreation.svelte
+++ client/src/components/SituationCreation.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import { Button, Loading, TextField } from 'attractions'
 	import api from '../api'
 	import { playerStore } from '../store/player_store'
@@ -8,22 +8,34 @@
 	let isCreated = false
 	let isLoading = false
 
+	$: isDisabled = isCreated || isLoading
+
 	async function createSituation() {
 		isLoading = true
 		await api.createSituation(situation).catch((e) => alert(e))
 		isCreated = true
 		isLoading = false
 	}
+
+	async function handleOnKeyDown(event: CustomEvent) {
+		if (!isDisabled && event.detail.nativeEvent.key === 'Enter') await createSituation()
+	}
 </script>
 
-<br />
 {#if $roomStore.isSituationCreator($playerStore.name)}
-	<div class="form">
-		<TextField placeholder="Some situation or funny thing" bind:value={situation} maxlength="255" />
-		<Button disabled={isLoading || isCreated} filled on:click={createSituation}
-			>{#if isLoading}<Loading />{:else}Create situation{/if}</Button
-		>
-	</div>
+	{#if !isDisabled}
+		<div class="form">
+			<TextField
+				placeholder="Some situation or funny thing"
+				bind:value={situation}
+				on:keydown={handleOnKeyDown}
+				maxlength={255}
+			/>
+			<Button disabled={isLoading || isCreated} filled on:click={createSituation}
+				>{#if isLoading}<Loading />{:else}Create situation{/if}</Button
+			>
+		</div>
+	{/if}
 {:else}
 	Wait for {$roomStore.round?.situation_creator_name} to create situation
 {/if}
